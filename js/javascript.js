24eb69e2f0039ae5c7dc707dd50922bb41df81b3
lordblumby <josh.blumberg@live.com>
2014-03-25 10:08:15 -0400

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,13 @@
 //called from onChange
 function check(input) {
-	if (document.getElementById('email1') != document.getElementById('email1check').value) {
+	if (document.getElementById('email1').value != input) {
 		input.setCustomValidity("The two lead's email addresses must match!")
 	} else {
 		//input is valid -- reset the error message
 		input.setCustomValidity('');
 	}
 	
-	if (document.getElementById('email2') != document.getElementById('email2check').value) {
+	if (document.getElementById('email2').value != input) {
 		input.setCustomValidity("The two follow's email addresses must match!")
 	} else {
 		//input is valud -- reset the error message
