8772fac120e87dfd92432596941fb69c2cc1545f
lordblumby <josh.blumberg@live.com>
2014-03-25 09:46:34 -0400

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,16 @@
 //called from onChange
 function check(input) {
-	if (input.value != document.getElementById('email_addr').value) {
-		input.setCustomValidity('The two email addresses must match.')
+	if (document.getElementById('email1') != document.getElementById('email1check').value) {
+		input.setCustomValidity("The two lead's email addresses must match!")
 	} else {
 		//input is valid -- reset the error message
+		input.setCustomValidity('');
+	}
+	
+	if (document.getElementById('email2') != document.getElementById('email2check').value) {
+		input.setCustomValidity("The two follow's email addresses must match!")
+	} else {
+		//input is valud -- reset the error message
 		input.setCustomValidity('');
 	}
 }
