8af1ab2bac055a23d593f875ea2e986b4f1edaf0
Lawrence Yong <theyong@umich.edu>
2014-03-25 09:30:31 -0400

    organization

--- javascript.js
+++
@@ -1,9 +0,0 @@
-//called from onChange
-function check(input) {
-    if (input.value != document.getElementById('email_addr').value) {
-        input.setCustomValidity('The two email addresses must match.') }
-    else {
-        //input is valid -- reset the error message
-        input.setCustomValidity('');
-    }
-}
