4e6beabe24cd760a767dc1db3b62a0ccc867b169
lordblumby <josh.blumberg@live.com>
2014-03-25 20:53:09 -0400

    Still trying to make the error msg werk

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,7 @@
 function checklead(laddress2) {
 	laddress1 = $('[name="email1"]');
 	if (laddress2.value != laddress1.val()) {
-		$("#leaderror").html("The lead's email addresses must match!");
+		$("#leaderror").html("The lead's email addresses must match!<br/>");
 	} else {
 		//input is valid -- reset the error message
 		$("#leaderror").html("");
@@ -12,7 +12,7 @@ function checklead(laddress2) {
 function checkfollow(faddress2) {
 	faddress1 = $('[name="email2"]');
 	if (faddress2.value != faddress1.val()) {
-		$("#followerror").html("The two follow's email addresses must match!")
+		$("#followerror").html("The two follow's email addresses must match<br/>!")
 	} else {
 		//input is valud -- reset the error message
 		$("#folowerror").html("");
