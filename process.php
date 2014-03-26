c262ba27f5673c2ebf1599768cdb84bc8969e9fb
lordblumby <josh.blumberg@live.com>
2014-03-26 18:32:01 -0400

    Redo on the 2 form thing
    The first way I did was silly of me...
    Now there's form1.html and form2.html.
    - form1.html has it's styling in style1.css.
    - form2.html has it's styling in style2.css
    
    Apologies for the ridiculous file copying before...that was dumb

--- process.php
+++ process.php
@@ -17,4 +17,16 @@
     fwrite($handle, $fileInfo);
     //now close the file
     fclose($handle);
+
+    //Send comfirmation email to the user
+	$msg = "Thank you for your info";//EMail message
+	
+	foreach($_POST as $name => $value) {
+		print "$name : $value<br/>";
+		$msg .="$name : $value\n";
+	} //the dot before the "=" concatonates
+	$to = "josh.blumberg@live.com";//Put in your email here
+	$headers = "From: ". $_POST["name"] ."<".$_POST["email"]. ">\r\n";	
+	mail($to, 'Registration', $msg,$headers);
+	echo "Email sent";
 ?>
